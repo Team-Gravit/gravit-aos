dd96bdc19ac8317c2c1adbe75c12fb81f6115571
rang1013 <rbfkd1013@naver.com>
2025-08-02 01:15:36 +0900

    리그 화면 구성

--- app/src/main/java/com/example/gravit/main/League/LeagueScreen.kt
+++ app/src/main/java/com/example/gravit/main/League/LeagueScreen.kt
@@ -1,9 +1,288 @@
 package com.example.gravit.main.League
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 
-fun LeagueScreen(){
+fun LeagueScreen() {
+    Box(modifier = Modifier.fillMaxWidth()){
+        Column(modifier = Modifier.fillMaxSize()) {
+            Surface(
+                shadowElevation = 5.dp,
+                color = Color.White
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                ) {
+                    Row(modifier = Modifier.fillMaxSize(),
+                        verticalAlignment = Alignment.CenterVertically,) {
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Text(
+                            text = "##등",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Box(
+                            modifier = Modifier
+                                .size(30.dp)
+                                .background(Color.LightGray)
+                                .clip(CircleShape)
+                        ) //티어 사진
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Column {
+                            Row {
+                                Text(text = "LV ##")
+                                Spacer(modifier = Modifier.width(16.dp))
+                                Text(text = "LP ####")
+                            }
+                            Text(
+                                text = "닉네임",
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+                }
+            }
 
+            Box(modifier = Modifier
+                .weight(3f)
+                .fillMaxSize()
+            ) // 티어 들어갈 부분
+
+            Box(modifier = Modifier
+                .weight(5f)
+                .fillMaxSize()
+            ){
+                Column(modifier = Modifier.fillMaxSize()){
+                    Box(
+                        modifier = Modifier
+                            .padding(15.dp) // 배경과의 거리
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .border(
+                                width = 1.dp,
+                                color = Color(0xFFDCDCDC),
+                                shape = RoundedCornerShape(16.dp)
+                            )
+                            .padding(10.dp),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = "##등",
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Spacer(modifier = Modifier.width(16.dp))
+                                Box(
+                                    modifier = Modifier
+                                        .size(30.dp)
+                                        .background(Color.LightGray)
+                                        .clip(CircleShape)
+                                )
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(
+                                    text = "닉네임",
+                                    fontSize = 15.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                            Column {
+                                Text(text = "LV ##")
+                                Text(text = "LP ####")
+                            }
+                        }
+                    }
+                    Box(
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .border(
+                                width = 1.dp,
+                                color = Color(0xFFDCDCDC),
+                                shape = RoundedCornerShape(16.dp)
+                            )
+                            .padding(10.dp),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = "##등",
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Spacer(modifier = Modifier.width(16.dp))
+                                Box(
+                                    modifier = Modifier
+                                        .size(30.dp)
+                                        .background(Color.LightGray)
+                                        .clip(CircleShape)
+                                )
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(
+                                    text = "닉네임",
+                                    fontSize = 15.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                            Column {
+                                Text(text = "LV ##")
+                                Text(text = "LP ####")
+                            }
+                        }
+                    }
+                    Box(
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .border(
+                                width = 1.dp,
+                                color = Color(0xFFDCDCDC),
+                                shape = RoundedCornerShape(16.dp)
+                            )
+                            .padding(10.dp),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = "##등",
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Spacer(modifier = Modifier.width(16.dp))
+                                Box(
+                                    modifier = Modifier
+                                        .size(30.dp)
+                                        .background(Color.LightGray)
+                                        .clip(CircleShape)
+                                )
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(
+                                    text = "닉네임",
+                                    fontSize = 15.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                            Column {
+                                Text(text = "LV ##")
+                                Text(text = "LP ####")
+                            }
+                        }
+                    }
+                    Box(
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .border(
+                                width = 1.dp,
+                                color = Color(0xFFDCDCDC),
+                                shape = RoundedCornerShape(16.dp)
+                            )
+                            .padding(10.dp),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = "##등",
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Spacer(modifier = Modifier.width(16.dp))
+                                Box(
+                                    modifier = Modifier
+                                        .size(30.dp)
+                                        .background(Color.LightGray)
+                                        .clip(CircleShape)
+                                )
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(
+                                    text = "닉네임",
+                                    fontSize = 15.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                            Column {
+                                Text(text = "LV ##")
+                                Text(text = "LP ####")
+                            }
+                        }
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(100.dp))
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(Color.LightGray)
+                .align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LeagueScreenPreview() {
+    LeagueScreen()
 }
--- app/src/main/java/com/example/gravit/main/Study/StudyScreen.kt
+++ app/src/main/java/com/example/gravit/main/Study/StudyScreen.kt
@@ -1,8 +1,12 @@
 package com.example.gravit.main.Study
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.gravit.ui.theme.pretendard
 
 @Composable
 fun StudyScreen(){
-
 }
