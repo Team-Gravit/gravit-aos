85f78c5f2b6de5572b89a18b230b345b25b97783
dyenab <qwpo01010@gmail.com>
2026-06-20 13:08:53 +0900

    refactor: 파일명 변경에 따른 import 수정

--- app/src/main/java/com/example/gravit/navigation/MainScreen.kt
+++ app/src/main/java/com/example/gravit/navigation/MainScreen.kt
@@ -41,7 +41,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import com.example.gravit.main.User.Notice.Notice2
-import com.example.gravit.main.User.Support.CustomerSupport
+import com.example.gravit.main.User.Inquiry.CustomerSupport
 import com.inuappcenter.gravit.main.User.MyPage
 
 
