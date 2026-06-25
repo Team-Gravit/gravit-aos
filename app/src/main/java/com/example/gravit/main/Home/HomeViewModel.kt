8cd8c2411995e59a45684342a4d478cb5e36f3b5
dyenab <qwpo01010@gmail.com>
2026-06-25 15:03:19 +0900

    ehance: 불필요한 호출 제거

--- app/src/main/java/com/example/gravit/main/Home/HomeViewModel.kt
+++ app/src/main/java/com/example/gravit/main/Home/HomeViewModel.kt
@@ -1,14 +1,12 @@
 package com.inuappcenter.gravit.main.Home
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.inuappcenter.gravit.api.ApiService
 import com.inuappcenter.gravit.api.AuthPrefs
 import com.inuappcenter.gravit.api.MainPageResponse
-import com.inuappcenter.gravit.api.UnitDetailResponses
 import com.inuappcenter.gravit.error.handleApiFailure
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -20,19 +18,15 @@ class HomeViewModel(
 ) : ViewModel() {
 
     sealed interface UiState {
+        data object Idle: UiState
         data object Loading : UiState
-
-        data class Success(
-            val data: MainPageResponse,
-            val units: List<UnitDetailResponses> = emptyList()
-        ) : UiState
-
+        data class Success(val data: MainPageResponse) : UiState
         data object Failed : UiState
         data object SessionExpired : UiState
         data object NotFound : UiState
     }
 
-    private val _state = MutableStateFlow<UiState>(UiState.Loading)
+    private val _state = MutableStateFlow<UiState>(UiState.Idle)
     val state = _state.asStateFlow()
 
     fun load() = viewModelScope.launch {
@@ -48,33 +42,11 @@ class HomeViewModel(
 
         runCatching {
             val mainData = api.getMainPage(auth)
-
-            val recentChapterId = mainData.learningDetailResponse.recentSolvedChapterId
-
-            val units =
-                if (recentChapterId != 0) {
-                    api.getUnitPage(
-                        auth = auth,
-                        chapterId = recentChapterId
-                    ).unitDetailResponses
-                } else {
-                    emptyList()
-                }
-
-            UiState.Success(
-                data = mainData,
-                units = units
-            )
+            UiState.Success(data = mainData)
         }.onSuccess { successState ->
             _state.value = successState
 
         }.onFailure { e ->
-            Log.e(
-                "HomeVM",
-                "load failed: ${e::class.simpleName}, ${e.message}",
-                e
-            )
-
             handleApiFailure(
                 e = e,
                 appContext = appContext,
