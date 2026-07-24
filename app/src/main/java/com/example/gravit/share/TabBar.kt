47a0293c30521c83bb5ef23322fee61adf35a366
dyenab <qwpo01010@gmail.com>
2026-07-24 22:18:23 +0900

    fix: private 제거, 접근성 수정

--- app/src/main/java/com/example/gravit/share/TabBar.kt
+++ app/src/main/java/com/example/gravit/share/TabBar.kt
@@ -9,17 +9,20 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import com.example.gravit.ui.theme.AppColor
 import com.example.gravit.ui.theme.AppTypography
 
 @Composable
-private fun TabBar(
+fun TabBar(
     selectedIndex: Int,
     onTabSelected: (Int) -> Unit,
     tab1: String,
@@ -34,7 +37,8 @@ private fun TabBar(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(40.dp)
-                .padding(horizontal = 16.dp),
+                .padding(horizontal = 16.dp)
+                .selectableGroup(),
             verticalAlignment = Alignment.CenterVertically
         ) {
             TabItem(
@@ -84,14 +88,14 @@ private fun TabItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val interactionSource = remember { MutableInteractionSource() }
     Box(
         modifier = modifier
             .fillMaxHeight()
-            .clickable(
-                interactionSource = interactionSource,
-                indication = null
-            ) { onClick() },
+            .selectable(
+                selected = selected,
+                onClick = onClick,
+                role = Role.Tab
+            ),
         contentAlignment = Alignment.Center
     ) {
         Text(
