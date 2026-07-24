7d370a07c6104f771259fcd8008cb65ded08d589
dyenab <qwpo01010@gmail.com>
2026-07-24 17:21:44 +0900

    fix: TopBar Icon 없는 버전 추가, 디자인 시스템 적용

--- app/src/main/java/com/example/gravit/main/User/TopBar.kt
+++ app/src/main/java/com/example/gravit/main/User/TopBar.kt
@@ -3,8 +3,6 @@ package com.inuappcenter.gravit.main.User
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.HorizontalDivider
@@ -23,20 +20,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.inuappcenter.gravit.ui.theme.pretendard
+import com.example.gravit.ui.theme.AppColor
+import com.example.gravit.ui.theme.AppTypography
 import com.inuappcenter.gravit.R
 
 @Composable
 fun TopBar(
     navController: NavController,
     title: String,
-    height: Dp = 80.dp,
+    height: Dp = 48.dp,
+    useIcon: Boolean = true,
     useCloseIcon: Boolean = false
 ) {
     Box(
@@ -46,50 +42,47 @@ fun TopBar(
             .height(height)
             .background(Color.White)
     ) {
-
-        if(useCloseIcon){
-            Icon(
-                imageVector = Icons.Default.Close,
-                contentDescription = "닫기",
-                modifier = Modifier
-                    .align(Alignment.CenterStart)
-                    .padding(start = 16.dp)
-                    .clickable {
-                        navController.popBackStack()
-                    },
-                tint = Color(0xFF4D4D4D)
-            )
-        } else {
-            Icon(
-                painter = painterResource(id = R.drawable.chevron_left),
-                contentDescription = "뒤로가기",
-                modifier = Modifier
-                    .align(Alignment.CenterStart)
-                    .padding(start = 12.dp)
-                    .size(24.dp)
-                    .clickable {
-                        navController.popBackStack()
-                    },
-                tint = Color.Black
-            )
+        if(useIcon){
+            if(useCloseIcon){
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "닫기",
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 16.dp)
+                        .clickable {
+                            navController.popBackStack()
+                        },
+                    tint = Color(0xFF4D4D4D)
+                )
+            } else {
+                Icon(
+                    painter = painterResource(id = R.drawable.chevron_left),
+                    contentDescription = "뒤로가기",
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(12.dp)
+                        .size(24.dp)
+                        .clickable {
+                            navController.popBackStack()
+                        },
+                    tint = AppColor.icon_default
+                )
+            }
         }
 
         if (title.isNotEmpty()) {
             Text(
                 text = title,
                 modifier = Modifier.align(Alignment.Center),
-                style = TextStyle(
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = pretendard
-                ),
-                color = Color(0xFF222222),
+                style = AppTypography.Label1,
+                color = AppColor.text2,
             )
         }
     }
 
     HorizontalDivider(
-        color = Color.Black.copy(alpha = 0.1f),
+        color = AppColor.divider1,
         thickness = 1.dp,
         modifier = Modifier.fillMaxWidth()
     )
