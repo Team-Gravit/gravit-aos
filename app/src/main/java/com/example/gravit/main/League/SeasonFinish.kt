37c40ca6bd2cf53be2b941035dd90b93d83cb9bf
dyenab <qwpo01010@gmail.com>
2026-09-08 18:48:08 +0900

    refactor: 변경된 디자인 적용

--- app/src/main/java/com/example/gravit/main/League/SeasonFinish.kt
+++ app/src/main/java/com/example/gravit/main/League/SeasonFinish.kt
@@ -1,87 +1,45 @@
 package com.inuappcenter.gravit.main.League
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.inuappcenter.gravit.ui.theme.mbc1961
-import com.inuappcenter.gravit.ui.theme.pretendard
+import com.example.gravit.ui.theme.AppColor
+import com.example.gravit.ui.theme.AppTypography
 import com.inuappcenter.gravit.R
 
 @Composable
-fun SeasonFinish(
-    seasonName: String?,
-){
-    val shadow = with(LocalDensity.current) {
-        androidx.compose.ui.graphics.Shadow(
-            color = Color.Black.copy(alpha = 0.25f),
-            offset = Offset(0f, 1.46.dp.toPx()),
-            blurRadius = 1.46.dp.toPx()
-        )
-    }
+fun SeasonFinish(){
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Box(
-            modifier = Modifier
-                .wrapContentSize()
-                .background(Color.White, shape = RoundedCornerShape(20.dp))
-                .clip(RoundedCornerShape(20.dp)),
-            contentAlignment = Alignment.Center
-        ) {
-            Column(
-                modifier = Modifier.padding(20.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "${seasonName} 종료",
-                    color = Color(0xFF8A00B8),
-                    style = TextStyle(
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Medium,
-                        fontFamily = mbc1961,
-                        shadow = shadow
-                    ),
-                )
-                Spacer(Modifier.height(10.dp))
-                Text(
-                    text = "시즌 정보를 집계중이에요!",
-                    fontFamily = pretendard,
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-                    color = Color(0xFF6D6D6D)
-                )
-            }
-        }
-        Spacer(Modifier.height(25.dp))
+        Text(
+            text = "시즌 정보를 집계중이에요!",
+            color = AppColor.text1w,
+            style = AppTypography.Title3
+        )
+        Spacer(Modifier.height(8.dp))
+        Text(
+            text = "이번 시즌도 수고했어요!",
+            style = AppTypography.Label2,
+            color = AppColor.text4
+        )
+        Spacer(Modifier.height(40.dp))
         Image(
             painter = painterResource(id = R.drawable.rabbit),
             contentDescription = null,
             modifier = Modifier.size(163.dp, 216.dp)
         )
-        Spacer(Modifier.height(60.dp))
     }
 }
