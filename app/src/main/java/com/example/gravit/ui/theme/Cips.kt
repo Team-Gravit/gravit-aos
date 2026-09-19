117ad1ccede9a3bc4275b29dca7260dd5b188865
dyenab <qwpo01010@gmail.com>
2026-09-19 20:30:37 +0900

    feat: clickable 없는 버전

--- app/src/main/java/com/example/gravit/ui/theme/Cips.kt
+++ app/src/main/java/com/example/gravit/ui/theme/Cips.kt
@@ -1,14 +1,16 @@
 package com.example.gravit.ui.theme
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -47,27 +49,41 @@ fun Cip(
         state == CipState.Active -> Color.Transparent
         else -> AppColor.CTA
     }
-
-    Button(
-        onClick = onClick,
-        modifier = modifier.defaultMinSize(
-            minWidth = 1.dp,
-            minHeight = 1.dp
-        ),
-        enabled = enabled,
-        shape = RoundedCornerShape(60.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = containerColor,
-            contentColor = contentColor
-        ),
-        border = BorderStroke(1.dp, borderColor),
-        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 0.dp)
-    ) {
-        if (text != null) {
-            Text(
-                text = text,
-                style = style
-            )
+    if(onClick == {}){
+        Box(
+            modifier = modifier
+                .clip(RoundedCornerShape(60.dp))
+                .background(containerColor)
+                .border(1.dp, borderColor, RoundedCornerShape(60.dp))
+                .clickable(enabled, onClick = onClick)
+                .padding(horizontal = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            if (text != null) {
+                Text(
+                    text = text,
+                    color = contentColor,
+                    style = style
+                )
+            }
+        }
+    }else {
+        Box(
+            modifier = modifier
+                .clip(RoundedCornerShape(60.dp))
+                .background(containerColor)
+                .border(1.dp, borderColor, RoundedCornerShape(60.dp))
+                .padding(horizontal = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            if (text != null) {
+                Text(
+                    text = text,
+                    color = contentColor,
+                    style = style
+                )
+            }
         }
     }
+
 }
